bf7d1b9e88e5179ee629979280b66dd4ba7943a3
Roy <royca311@gmail.com>
2021-02-14 15:35:40 +0530

    main.tf

---
+++ main.tf
@@ -0,0 +1,37 @@
+terraform {
+  required_version = ">= 0.12"
+  required_providers {
+    google = {
+        source = "hashicorp/google"
+        version = "3.5.0"
+    }
+  }
+}
+
+provider "google" {
+    credentials = file("gcp-project-1-25651a89c564.json")
+    project = "zinc-fusion-288207"
+    region = "us-central1"
+    zone = "us-central1-c"
+}
+
+resource "google_compute_network" "vpc_network" {
+    name = "terraform-network"
+}
+
+resource "google_compute_instance" "vm_instance" {
+    name = "terraform-instance"
+    machine_type ="f1-micro"
+    tags = ["web" , "dev"]
+    boot_disk {
+        initialize_params {
+            image = "cos-cloud/cos-stable"
+        }
+    }
+
+    network_interface {
+        network = google_compute_network.vpc_network.name
+        access_config {
+        }
+    }
+}
